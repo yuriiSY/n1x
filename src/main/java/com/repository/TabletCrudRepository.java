ada2de40dc00885ca0ce2c1b562cc62b81e7308c
Yurii <yurasykal@gmail.com>
2022-07-06 15:22:33 +0100

    hw1

---
+++ src/main/java/com/repository/TabletCrudRepository.java
@@ -0,0 +1,21 @@
+package com.repository;
+
+import com.model.Tablet;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TabletCrudRepository {
+
+    void save(Tablet tablet);
+
+    void saveAll(List<Tablet> tablets);
+
+    boolean update(Tablet tablet);
+
+    boolean delete(String id);
+
+    List<Tablet> getAll();
+
+    Optional<Tablet> findById(String id);
+}
